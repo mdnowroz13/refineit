012a8509455ce8b1db803bc7d533f446d73a7c3b
mdnowroz13 <mdnowroz13@gmail.com>
2025-11-30 20:14:22 +0530

    chore: add demo file for recording

---
+++ src/demo-recording.ts
@@ -0,0 +1,9 @@
+// This file is created to demonstrate RefineIt's auto-fix capability.
+// It contains unused variables that should be detected and fixed/reported.
+
+export function demoFunction() {
+    const unusedVar = "I am not used anywhere";
+    const anotherUnused = 42;
+
+    console.log("Hello from demo function");
+}
